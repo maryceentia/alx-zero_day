06e4d7abaca0eebce2b1679e3a6096e782f1123c
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2022-10-20 05:33:30 +0100

    creates a file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # puppet script that creates a file in /tmp
 
 file { '/tmp/school':
-  content => 'I love puppet',
+  content => 'I love Puppet',
   mode    =>  '0744',
   owner   => 'www-data',
   group   => 'www-data',
