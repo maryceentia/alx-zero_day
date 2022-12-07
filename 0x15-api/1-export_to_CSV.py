0c227db18f74f2fb18b3ba1dc60ab45bd0cc95f9
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2022-12-07 23:08:27 +0100

    export data in the CSV format.

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""returns information about user TODO list progress using employee ID"""
+import csv
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com'
+    user = requests.get(url + 'users', params={'id': sys.argv[1]})
+    data = requests.get(url + 'todos', params={'userId': sys.argv[1]})
+
+    with open(f'{sys.argv[1]}.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        for d in data.json():
+            row = f"{d['userId']}, {user.json()[0]['username']},\
+                    {d['completed']}, {d['title']}"
+            writer.writerow(row)
