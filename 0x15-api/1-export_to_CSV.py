333a7bf12e12e410e2b750c58d05eec01cc8e1ac
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2022-12-08 12:18:00 +0100

    export employee info to csv format.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,13 +5,13 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com'
+    url = 'https://jsonplaceholder.typicode.com/'
     user = requests.get(url + 'users', params={'id': sys.argv[1]})
     data = requests.get(url + 'todos', params={'userId': sys.argv[1]})
 
     with open(f'{sys.argv[1]}.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         for d in data.json():
-            row = f"{d['userId']}, {user.json()[0]['username']},\
-                    {d['completed']}, {d['title']}"
-            writer.writerow(row)
+           row = (d['userId'], user.json()[0]['username'],\
+                    d['completed'], d['title'])
+           writer.writerow(row)
