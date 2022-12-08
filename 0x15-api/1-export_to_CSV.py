d832d1f63b257ba559dd6dc19dd8b331fbb302cc
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2022-12-08 19:10:17 +0100

    export employee info to csv format.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,14 +4,15 @@ import csv
 import requests
 import sys
 
-if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    user = requests.get(url + 'users', params={'id': sys.argv[1]})
-    data = requests.get(url + 'todos', params={'userId': sys.argv[1]})
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open(f'{sys.argv[1]}.csv', 'w', newline='') as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-        for d in data.json():
-           row = (d['userId'], user.json()[0]['username'],\
-                    d['completed'], d['title'])
-           writer.writerow(row)
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
